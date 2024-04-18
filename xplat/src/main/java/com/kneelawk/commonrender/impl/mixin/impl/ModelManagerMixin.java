d722ef9483a6b291595074ddb0846e5e300858d9
Kneelawk <kneelawk@gmail.com>
2024-04-18 11:42:57 -0700

    Add model-manager mixin

---
+++ xplat/src/main/java/com/kneelawk/commonrender/impl/mixin/impl/ModelManagerMixin.java
@@ -0,0 +1,9 @@
+package com.kneelawk.commonrender.impl.mixin.impl;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.resources.model.ModelManager;
+
+@Mixin(ModelManager.class)
+public class ModelManagerMixin {
+}
