8e2f078ae9d4709b12d232dfa86cdf0fbd005d19
Cyan Kneelawk <kneelawk@gmail.com>
2024-08-10 14:47:59 -0700

    Update submodule

--- examples/ct-complicated-neoforge/build.gradle.kts
+++ examples/ct-complicated-neoforge/build.gradle.kts
@@ -7,14 +7,3 @@ submodule {
     applyXplatConnection(":ct-complicated-xplat")
     generateRuns()
 }
-
-neoForge {
-    mods {
-        create("krender_model_loading") {
-            dependency(project(":model-loading-neoforge"))
-        }
-        create("krender_model_creation") {
-            dependency(project(":model-creation-neoforge"))
-        }
-    }
-}
