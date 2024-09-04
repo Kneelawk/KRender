46d69d99357d4d380e7213ae43dc6c9c7e6014f2
Cyan Kneelawk <kneelawk@gmail.com>
2024-09-03 20:48:23 -0700

    Add texture package-info

---
+++ modules/engine-api-xplat/src/main/java/com/kneelawk/krender/engine/api/texture/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * KRender Engine texture utilities and interfaces.
+ */
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.kneelawk.krender.engine.api.texture;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
