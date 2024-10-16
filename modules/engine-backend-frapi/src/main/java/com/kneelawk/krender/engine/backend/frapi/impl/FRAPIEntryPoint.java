e7c001b0539c56bc801e9dd1c64a6745d3260838
Cyan Kneelawk <kneelawk@gmail.com>
2024-10-16 05:49:01 -0700

    Make FRAPI backend compatible with FFRAPI

--- modules/engine-backend-frapi/src/main/java/com/kneelawk/krender/engine/backend/frapi/impl/FRAPIEntryPoint.java
+++ modules/engine-backend-frapi/src/main/java/com/kneelawk/krender/engine/backend/frapi/impl/FRAPIEntryPoint.java
@@ -8,7 +8,7 @@ import com.kneelawk.krender.engine.api.backend.BackendRegistrationCallback;
 public class FRAPIEntryPoint {
     @Listen(BackendRegistrationCallback.class)
     public static void register(BackendRegistrationCallback.Context ctx) {
-        if (ctx.isModLoaded("fabric-renderer-api-v1")) {
+        if (ctx.isModLoaded("fabric-renderer-api-v1") || ctx.isModLoaded("fabric_renderer_api_v1")) {
             ctx.registerBackend("com.kneelawk.krender.engine.backend.frapi.impl.FRAPIPredicate",
                 "com.kneelawk.krender.engine.backend.frapi.impl.FRAPIBackend");
         }
