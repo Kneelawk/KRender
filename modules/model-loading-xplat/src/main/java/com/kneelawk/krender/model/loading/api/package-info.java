4fd8ce6ad89cebb3fda337025b5d93f6243311a6
Cyan Kneelawk <kneelawk@gmail.com>
2024-10-19 03:42:56 -0700

    Add package-info to model-loading lib

---
+++ modules/model-loading-xplat/src/main/java/com/kneelawk/krender/model/loading/api/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * KRender Model Loading Library.
+ * <p>
+ * This library allows mods to hook into the {@link net.minecraft.client.resources.model.ModelBakery} using
+ * {@link com.kneelawk.krender.model.loading.api.ModelBakeryPlugin}s and
+ * {@link com.kneelawk.krender.model.loading.api.PreparableModelBakeryPlugin}s.
+ */
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.kneelawk.krender.model.loading.api;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
